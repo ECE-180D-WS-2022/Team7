a1af6ff843a0e7bba4cec6f26b348a1b68cb1b6a
make-a-mark <56522555+make-a-mark@users.noreply.github.com>
2022-03-07 15:32:45 -0800

    fix printing

--- Bruin Pong/main_game.py
+++ Bruin Pong/main_game.py
@@ -300,9 +300,9 @@ class Connection:
                                     #     continue
                                     if max_x != self.velocity:
                                         self.velocity = max_x
-                                    print('velocity = ', self.velocity)
-                                    global mqtt_client
-                                    mqtt_client.publish('ece180d/team7/pygame', self.velocity[0], qos=1)
+                                        print('velocity = ', self.velocity)
+                                        global mqtt_client
+                                        mqtt_client.publish('ece180d/team7/pygame', self.velocity[0], qos=1)
 
                 # while True:
                 #     if not self.connected:
