075b330675b4b3e4ee4867df60c80d2515590a16
Andrew <andrewl.shopping@gmail.com>
2022-05-26 15:33:08 -0700

    updated main page

--- Bruin Pong/bruinpong.py
+++ Bruin Pong/bruinpong.py
@@ -564,14 +564,14 @@ player_stand_rect = player_stand.get_rect(center = (600,500))
 game_name = font_size(150).render('Bruin Pong',False, BLACK)
 game_name_rect = game_name.get_rect(center = (600,200))
 
-game_message = test_font.render('Press SPACE for Single Player',False,BLACK)
-game_message_rect = game_message.get_rect(center = (400,330))
+game_message = test_font.render('Press "SPACE" or say "Single" for Single Player',False,BLACK)
+game_message_rect = game_message.get_rect(center = (500,330))
 
-game_message2 = test_font.render('Press M for Multiplayer',False,BLACK)
-game_message_rect2 = game_message.get_rect(center = (400,400))
+game_message2 = test_font.render('Press "M" or say "Multi" for Multiplayer',False,BLACK)
+game_message_rect2 = game_message.get_rect(center = (500,400))
 
-game_message3 = test_font.render('Press R for Rules',False,BLACK)
-game_message_rect3 = game_message.get_rect(center = (400,470))
+game_message3 = test_font.render('Press "R" for Rules',False,BLACK)
+game_message_rect3 = game_message.get_rect(center = (500,470))
 
 # Timer
 ball_timer = pygame.USEREVENT + 1
